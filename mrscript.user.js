19b40ac282a192bd50eebe74724a254e4a4941b8
Hellion <hellion.e@gmail.com>
2010-10-06 09:40:51 -0500

    update to 1.5.9

--- mrscript.user.js
+++ mrscript.user.js
@@ -1,4 +1,4 @@
-// Mr. Script v1.5.8
+// Mr. Script v1.5.9
 //
 // --------------------------------------------------------------------
 // This is a user script.  To install it, you need Greasemonkey 0.8 or
@@ -11,7 +11,7 @@
 // ==UserScript==
 // @name        Mr. Script
 // @namespace   http://www.noblesse-oblige.org/lukifer/scripts/
-// @description Version 1.5.8
+// @description Version 1.5.9
 // @author		Lukifer
 // @contributor	Ohayou
 // @contributor Hellion
@@ -34,7 +34,7 @@ var place = location.pathname.replace(/\/|\.(php|html)$/gi, "").toLowerCase();
 //GM_log("at:" + place);
 
 // n.b. version number should always be a 3-digit number.  If you move to 1.6, call it 1.6.0.  Don't go to 1.5.10 or some such.
-var VERSION = 158;
+var VERSION = 159;
 var MAXLIMIT = 999;
 var ENABLE_QS_REFRESH = 1;
 var DISABLE_ITEM_DB = 0;
