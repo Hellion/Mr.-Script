f348d130d49baeae604868c171f9dcee6a7878b9
Hellion <hellion.e@gmail.com>
2010-11-04 18:23:22 -0500

    progressbar tweak; refactor nemesis quest stuff slightly

--- mrscript.user.js
+++ mrscript.user.js
@@ -2115,41 +2115,41 @@ function at_guild() {
 //			if (tdtext.indexOf("You acquire an item: Fernswarthy's key") != -1) {
 //				$('b:eq(1)').append(AppendLink("[Fern's Tower]",'fernruin.php'));
 //			} else 
-			if  ((tdtext.indexOf("brought me Fernswarthy's key") != -1) || 	// AT
-				 (tdtext.indexOf("Misspelled Cemetary") != -1) || 			// SC/TT
-				 (tdtext.indexOf("brought the key to") != -1) ||			// PM/SA
-				 (tdtext.indexOf("haven't got Fern") != -1)) 				// DB		whew.  stupid flavor text.
+			if  ((tdtext.indexOf("brought me Fernswarthy's key") != -1) || 	// ?  stupid wiki, lacking data.
+				 (tdtext.indexOf("Misspelled Cemetary") != -1) || 			// SC/TT?
+				 (tdtext.indexOf("haven't got Fern") != -1) ||				// AT	
+				 (tdtext.indexOf("brought the key to") != -1))				// PM/SA?
 			{		
 				td.append(AppendLink('[Misspelled Cemetary (1)]','adventure.php?snarfblat=21'));
-			} else if (tdtext.indexOf("searching the ruins") != -1) {
+			} else if ((tdtext.indexOf("searching the ruins") != -1) ||		// other?
+				 (tdtext.indexOf("searching the tower") != -1))				// DB
+			{
 				td.append(AppendLink('[Tower Ruins (1)]','adventure.php?snarfblat=22'));
 			}
 		break;
 		case "?place=scg": 
 			GM_log("tdtext="+tdtext);
-			if ((tdtext.indexOf("the two oldest and wisest") != -1) || 	// common opening phrase, yay.
-				(tdtext.indexOf("completed your Epic") != -1) ||		// Mus/Mys interstitial
-				(tdtext.indexOf("delay on that epic") != -1))			// Mox interstitial
+			if (tdtext.indexOf("the two oldest and wisest") != -1) 		// common opening phrase, yay.
 			{
-				if ($('p').length) {									// opening uses <p> tags
-					$('p:last').append(AppendLink('[hermit]','hermit.php')).append(AppendLink('[casino]','casino.php'));
-				} else {												// interstitial uses naked <td>.  feh.
-					td.append(AppendLink('[hermit]','hermit.php')).append(AppendLink('[casino]','casino.php'));
-				}
-			} else if (
-				(tdtext.indexOf("Beelzebozo") != -1) ||						// EW->LEW assignment, all classes.
-				(tdtext.indexOf("restore the Legendary") != -1) || 			// EW->LEW muscle inter
-				(tdtext.indexOf("acquire the Legendary") != -1) || 			// EW->LEW myst inter
-				(tdtext.indexOf("with that Legendary") != -1))				// EW-LEW mox inter
+				$('p:last').append(AppendLink('[hermit]','hermit.php')).append(AppendLink('[casino]','casino.php'));
+			} else if ((tdtext.indexOf("completed your Epic") != -1) ||		// Mus/Mys interstitial
+					   (tdtext.indexOf("delay on that epic") != -1))			// Mox interstitial
+			{															
+				td.append(AppendLink('[hermit]','hermit.php')).append(AppendLink('[casino]','casino.php')); // interstitial uses naked <td>.  feh.
+			} else if (tdtext.indexOf("Beelzebozo") != -1) 						// EW->LEW assignment, all classes.
 			{
-				if ($('p').length) $('p:last').append(AppendLink('[Fun House (1)]','adventure.php?snarfblat=20'));
-				else td.append(AppendLink('[Fun House (1)]','adventure.php?snarfblat=20'));
-			} else if (
-				(tdtext.indexOf("on your map") != -1) ||					// Cave assignment, all classes
-				(tdtext.indexOf("defeated your Nemesis yet") != -1) ||		// Muscle inter
-				(tdtext.indexOf("need you to defeat") != -1) ||				// Myst inter
-				(tdtext.indexOf("beat your Nemesis yet, eh?") != -1))		// Moxie inter
-				)
+				$('p:last').append(AppendLink('[Fun House (1)]','adventure.php?snarfblat=20'));
+			} else if ((tdtext.indexOf("restore the Legendary") != -1) || 			// EW->LEW muscle inter
+					   (tdtext.indexOf("acquire the Legendary") != -1) || 			// EW->LEW myst inter
+					   (tdtext.indexOf("with that Legendary") != -1))				// EW-LEW mox inter
+			{
+				td.append(AppendLink('[Fun House (1)]','adventure.php?snarfblat=20'));
+			} else if (tdtext.indexOf("on your map") != -1) 				// Cave assignment, all classes
+			{
+				$('p:last').append(AppendLink('[nemesis cave]','cave.php'));
+			} else if ((tdtext.indexOf("defeated your Nemesis yet") != -1) ||		// Muscle inter
+				(tdtext.indexOf("need you to defeat") != -1) ||						// Myst inter
+				(tdtext.indexOf("beat your Nemesis yet, eh?") != -1))				// Moxie inter
 			{
 				td.append(AppendLink('[nemesis cave]','cave.php'));
 			} else if (tdtext.indexOf("volcano lair or something") != -1) {	// all classes: start of assassin encounters
@@ -3545,13 +3545,16 @@ function at_questlog()
 
 function fix_progressbar(totalWidth, level)
 {
-	var levelbar = $('table[title]:first').get(0);
+	var levelbar = $('table[title]:first');
+	if (!levelbar.length) return;	// if there's no level progress bar, we have nothing to do here.
+	levelbar = levelbar.get(0);
+	var statbars = $('td[align="left"]');
+	if (!statbar.length) return;	// can't do the right thing without the stat bars...
 	var mainstatProgBarCount = parseInt(levelbar.title.match(/\d+/)[0]); // = how many stat points into this level we are
 	var charclass = $('table center').contents().filter(function() {if (this.nodeType == 3) return true;}).get(1);
 	if (charclass) charclass = charclass.data; 
 	// should return the "class" part of "<table><center><tr><td><b>name</b><br>level X<br>class<blah...>" in full mode.
 	GM_log("charclass="+charclass);
-	var statbars = $('td[align="left"]');
 	var mainstatbar = false;
 	if ((charclass == "Turtle Tamer") || (charclass == "Seal Clubber")) mainstatbar = statbars.get(0);
 	if ((charclass == "Pastamancer") || (charclass == "Sauceror")) mainstatbar = statbars.get(1);
